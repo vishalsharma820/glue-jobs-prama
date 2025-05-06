49dd0d6f0f3c6d69b61b3bc008c4c5eef3ecadee
vishal.sharma <vishal.sharma@prama.ai>
2025-05-06 15:48:38 +0530

    removed old files

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-glue-etl-tf-state"
-    key            = "etl/terraform.tfstate"
-    region         = "us-east-1"
-    # dynamodb_table = "glue-terraform-locks"
-    # encrypt        = true
-  }
-}
